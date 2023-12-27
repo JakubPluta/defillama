ec920317111b8374da70ac13b85ef1d83b88103d
jp <plutakuba@gmail.com>
2023-12-27 13:10:05 +0100

    feat: added helper methods to handle get requests

--- defillama/client.py
+++ defillama/client.py
@@ -1,3 +1,5 @@
+import enum
+from typing import Any, Dict, List
 import requests
 import pprint
 from exc import InvalidResponseDataException, InvalidResponseStatusCodeException
@@ -7,25 +9,56 @@ from log import get_logger
 
 log = get_logger(__name__)
 
+class ApiSectionsEnum(str, enum.Enum):
+    TVL = "tvl"
+    COINS = "coins"
+    STABLECOINS = "stablecoins"
+    YIELDS = "yields"
+    BRIDGES = "bridges"
+    VOLUMES = "volumes"
+    FEES = "fees"
 
 class DefiLlamaClient:
     def __init__(self, **kwargs) -> None:
-        self._base_url: str = "https://api.llama.fi"
-        self._stablecoins_url: str = "https://stablecoins.llama.fi"
+        self._urls = {
+            "tvl": "https://api.llama.fi",
+            "coins": "https://coins.llama.fi",
+            "stablecoins": "https://stablecoins.llama.fi",
+            "yields": "https://yields.llama.fi",
+            "bridges": "https://bridges.llama.fi",
+            "volumes": "https://api.llama.fi",
+            "fees": "https://api.llama.fi",
+            
+            
+        }
         self._session: requests.Session = get_retry_session()
 
         if "headers" in kwargs:
             self._session.headers.update(kwargs["headers"])
+        
+    def _resolve_api_url(self, section: str) -> str:
+        if section not in self._urls.keys():
+            raise ValueError(f"Invalid section: {section}")
+        return self._urls[section]
+    
+    def _build_endpoint_url(self, section: str, endpoint: str, *args) -> str:
+        url = f"{self._resolve_api_url(section)}/{endpoint}"
+        if args:
+            url += "/" + "/".join(args)
+        return url
 
     @property
     def session(self) -> requests.Session:
         return self._session
 
-    def _format_url(self, endpoint: str) -> str:
-        return f"{self._base_url}/{endpoint}"
 
-    def _get(self, endpoint: str, **kwargs) -> requests.Response:
-        return self._session.get(self._format_url(endpoint), **kwargs)
+    def _get(self, section: ApiSectionsEnum, endpoint: str, *args, **query_params) -> requests.Response:
+        r = self.session.get(
+            self._build_endpoint_url(section, endpoint, *args),
+            params=query_params,
+            
+        )
+        return self._handle_response(r)
 
     def _handle_response(self, response: requests.Response) -> dict:
         """
@@ -47,29 +80,69 @@ class DefiLlamaClient:
             raise InvalidResponseDataException(f"Invalid data: {response.text}") from ve
         return data
 
-    def get_protocols(self):
-        r = self.session.get(f"{self._base_url}/protocols")
-        return self._handle_response(r)
+    def get_protocols(self) -> List[Dict[Any, Any]]:
+        """List all protocols on defillama along with their tvl."""
+        return self._get(ApiSectionsEnum.TVL, "protocols")
+    
+    
+    def get_protocol(self, protocol: str) -> Dict[Any, Any]:
+        """Get historical TVL of a protocol and breakdowns by token and chain.
 
-    def get_protocol(self, protocol):
-        r = self.session.get(f"{self._base_url}/protocol/{protocol}")
-        return self._handle_response(r)
+        Parameters:
+            protocol (str): The protocol slug to retrieve.
 
-    def get_historical_tvl_for_all_chains(self):
-        r = self.session.get(f"{self._base_url}/v2/historicalChainTvl")
-        return self._handle_response(r)
+        Returns:
+            The historical TVL of the protocol and breakdowns by token and chain.
+        """
+        return self._get(ApiSectionsEnum.TVL, "protocol", protocol)
+    
+    
+    def get_historical_tvl_of_defi_on_all_chains(self) -> List[Dict[Any, Any]]:
+        """
+        Retrieves the historical total value locked (TVL) of decentralized finance (DeFi) on all chains.
 
-    def get_historical_tvl_for_chain(self, chain):
-        r = self.session.get(f"{self._base_url}/v2/historicalChainTvl/{chain}")
-        return self._handle_response(r)
+        Returns:
+            A list of dictionaries representing the historical TVL data for each chain. Each dictionary contains
+            the date and the corresponding TVL value.
+        """
+        return self._get(ApiSectionsEnum.TVL, "v2", "historicalChainTvl")
 
-    def get_historical_tvl_for_protocol(self, protocol):
-        r = self.session.get(f"{self._base_url}/tvl/{protocol}")
-        return self._handle_response(r)
+    def get_historical_tvl_for_chain(self, chain: str) -> List[Dict[Any, Any]]:
+        """
+        Returns the historical total value locked (TVL) for a specific chain.
 
-    def get_current_tvl_of_all_chains(self):
-        r = self.session.get(f"{self._base_url}/v2/chains")
-        return self._handle_response(r)
+        Parameters:
+            chain (str): chain slug, you can get these from /chains or the chains property on /protocols
+
+        Returns:
+            The historical TVL for the specified chain. The returned data is a list of dictionaries, where each
+            dictionary contains the date and the corresponding TVL value.
+        """
+        
+        return self._get(ApiSectionsEnum.TVL, "v2", "historicalChainTvl", chain)
+
+    def get_current_tvl_for_protocol(self, protocol: str) -> int:
+        """
+        Get the current total value locked (TVL) for a given protocol.
+
+        Parameters:
+            protocol (str): The protocol slug to retrieve.
+
+        Returns:
+            int: The current TVL for the specified protocol.
+        """
+        
+        return self._get(ApiSectionsEnum.TVL, "tvl", protocol)
+
+    def get_current_tvl_of_all_chains(self) -> List[Dict[Any, Any]]:
+        """Get the current total value locked (TVL) of all chains.  
+        
+        Returns:
+            The current TVL for all chains. The returned data is a list of dictionaries, where each dictionary contains
+            the chain name, id, token symbol and the corresponding TVL value.
+        
+        """
+        return self._get(ApiSectionsEnum.TVL, "v2", "chains")
 
     def get_stablecoins(self, include_prices: bool = True):
         "include prices -> query params"
@@ -119,3 +192,4 @@ class DefiLlamaClient:
             f"{self._stablecoins_url}/stablecoinprices",
         )
         return self._handle_response(r)
+
